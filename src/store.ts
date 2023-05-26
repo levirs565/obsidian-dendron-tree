e319a71d12aad1085d6ecfa4bb2099550693a183
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2023-05-26 15:24:52 +0700

    refactor: remove unused store

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,12 @@
 import { derived, get, writable } from "svelte/store";
-import type ExamplePlugin from "./main";
-import { Note } from "./note";
+import type DendronTreePlugin from "./main";
 import { TFile } from "obsidian";
 import { DendronVault } from "./dendron-vault";
 
-export const plugin = writable<ExamplePlugin>();
+export const plugin = writable<DendronTreePlugin>();
 export const getPlugin = () => get(plugin);
 
 export const activeFile = writable<TFile>();
-
-export const rootNote = writable<Note>();
-export const getRootNote = () => get(rootNote);
 
 export const dendronVaultList = writable<DendronVault[]>([]);
 export const getDendronVaultList = () => get(dendronVaultList);
